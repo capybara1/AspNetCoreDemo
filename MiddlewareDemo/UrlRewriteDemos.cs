87db4ab9be7156ee41833ccf18cd49c129f0e1b3
Jochen <jochen.handels@web.de>
2018-10-26 16:31:02 +0200

    Added redirect demos

---
+++ MiddlewareDemo/UrlRewriteDemos.cs
@@ -0,0 +1,96 @@
+﻿using AspNetCoreDemo.MiddlewareDemo.Contracts;
+using AspNetCoreDemo.MiddlewareDemo.Middlewares;
+using AspNetCoreDemo.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNetCoreDemo.MiddlewareDemo
+{
+    [Trait("Category", "ASP.NET Core Middleware / URL Rewrite")]
+    public class UrlRewriteDemos
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UrlRewriteDemos(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+        }
+
+        [Fact(DisplayName = "Use rewrite rule")]
+        public async Task UseRewriteRule()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureLogging(setup =>
+                {
+                    setup.AddDebug();
+                    setup.SetupDemoLogging(_testOutputHelper);
+                })
+                .Configure(app =>
+                {
+                    var options = new RewriteOptions()
+                        .AddRewrite(
+                            @"^rewritable/(\w+)",
+                            "rewritten?param=$1",
+                            skipRemainingRules: true);
+
+                    app.UseRewriter(options);
+
+                    app.Run(async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status200OK;
+                        context.Response.ContentType = "text/plain";
+                        await context.Response.WriteAsync(
+                            context.Request.Query["param"].First(),
+                            new UTF8Encoding(false));
+                    });
+                });
+
+            var server = new TestServer(builder);
+
+            var client = server.CreateClient();
+
+            var response = await client.GetAsync("rewritable/test");
+            Assert.Equal(200, (int)response.StatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal("test", content);
+        }
+
+        [Fact(DisplayName = "Use redirect rule")]
+        public async Task UseRedirectRule()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureLogging(setup =>
+                {
+                    setup.AddDebug();
+                    setup.SetupDemoLogging(_testOutputHelper);
+                })
+                .Configure(app =>
+                {
+                    var options = new RewriteOptions()
+                        .AddRedirect("redirectable/(.*)", "redirected/$1");
+
+                    app.UseRewriter(options);
+                });
+
+            var server = new TestServer(builder);
+
+            var client = server.CreateClient();
+
+            var response = await client.GetAsync("redirectable/test");
+            Assert.Equal(302, (int)response.StatusCode);
+            Assert.Equal("/redirected/test", response.Headers.Location.ToString());
+        }
+    }
+}
