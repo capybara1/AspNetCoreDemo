a49ba6a5e64e6e6dc6f4dd95e9b04f52534cdc12
Jochen <jochen.handels@web.de>
2018-11-01 22:38:35 +0100

    Added example for hosted service

---
+++ MiddlewareDemo/HostedServiceDemos.cs
@@ -0,0 +1,57 @@
+﻿using AspNetCoreDemo.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNetCoreDemo.MiddlewareDemo
+{
+    [Trait("Category", "ASP.NET Core Middleware / Hosted Servicer")]
+    public partial class HostedServiceDemos
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public HostedServiceDemos(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+        }
+
+        [Fact(DisplayName = "Setup hosted example service")]
+        public async Task SetupHostedExampleService()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .ConfigureLogging(setup =>
+                {
+                    setup.AddDebug();
+                    setup.SetupDemoLogging(_testOutputHelper);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Services.HostedExampleService>();
+                })
+                .Configure(app =>
+                {
+                    app.Run(async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status200OK;
+                        context.Response.ContentType = "text/plain";
+                        await context.Response.WriteAsync($"Hello, World!", new UTF8Encoding(false));
+                    });
+                });
+
+            var server = new TestServer(webHostBuilder);
+            using (server)
+            {
+                await Task.Delay(20);
+            }
+        }
+    }
+}
---
+++ MiddlewareDemo/Services/HostedExampleService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.MiddlewareDemo.Services
+{
+    public class HostedExampleService : IHostedService
+    {
+        private readonly ILogger<HostedExampleService> _logger;
+
+        private Timer _timer;
+
+        public HostedExampleService(ILogger<HostedExampleService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromMilliseconds(10));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Ping!");
+        }
+        
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
