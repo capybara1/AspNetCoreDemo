0aa0a09c0a4707262209ad17531593fd21eeeb1d
Jochen <jochen.handels@web.de>
2018-11-02 14:39:43 +0100

    Added API-Controller examples

---
+++ MvcDemo/ApiControllerDemos.cs
@@ -0,0 +1,103 @@
+﻿using AspNetCoreDemo.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNetCoreDemo.MvcDemo
+{
+    [Trait("Category", "ASP.NET Core MVC / API-Controller")]
+    public class ApiControllerDemos
+    {
+        // See also https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-2.1
+
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ApiControllerDemos(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+        }
+
+        [Fact(DisplayName = "Suppress specific behaviors of API-Controller")]
+        public async Task SuppressSpecificBehaviorsOfApiController()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureLogging(setup =>
+                {
+                    setup.AddDebug();
+                    setup.SetupDemoLogging(_testOutputHelper);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddMvcCore()
+                        .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                    services.Configure<ApiBehaviorOptions>(options =>
+                    {
+                        // Available but not used in this example
+                        // options.SuppressConsumesConstraintForFormFileParameters
+                        // options.SuppressInferBindingSourcesForParameters
+
+                        options.SuppressModelStateInvalidFilter = true;
+                    });
+
+                })
+                .Configure(app =>
+                {
+                    app.UseMvc();
+                });
+
+            var server = new TestServer(builder);
+
+            var client = server.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+
+            var response = await client.GetAsync("/apicontroller?param=invalid");
+
+            Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+        }
+
+        [Fact(DisplayName = "Use API-Controller to get response infos")]
+        public async Task UseApiControllerToGetResponseInfos()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureLogging(setup =>
+                {
+                    setup.AddDebug();
+                    setup.SetupDemoLogging(_testOutputHelper);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddMvcCore()
+                        .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                        .AddJsonFormatters();
+                    services.Configure<ApiBehaviorOptions>(options =>
+                    {
+                        options.InvalidModelStateResponseFactory = context =>
+                        {
+                            return new BadRequestObjectResult(context.ModelState);
+                        };
+                    });
+                })
+                .Configure(app =>
+                {
+                    app.UseMvc();
+                });
+
+            var server = new TestServer(builder);
+
+            var client = server.CreateClient();
+
+            var response = await client.GetAsync("/apicontroller?param=invalid");
+
+            Assert.Equal(StatusCodes.Status400BadRequest, (int)response.StatusCode);
+        }
+    }
+}
---
+++ MvcDemo/Controllers/ApiControllerExamplesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreDemo.MvcDemo.Controllers
+{
+    [Route("ApiController")]
+    [ApiController]
+    [Produces("application/json")] // Required to perform output formatting
+    public class ApiControllerExamplesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(404)]
+        public ActionResult<string> TestAction(int param)
+        {
+            if (param > 10)
+            {
+                return BadRequest();
+            }
+            return "Hello, World!";
+        }
+    }
+}
